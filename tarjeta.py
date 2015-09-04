12c864e89c62a3ad2212a29909171f8f65e0f7d4
VictoriaHuhn <vichuhn@gmail.com>
2015-09-04 17:23:47 -0300

    Update tarjeta.py

--- tarjeta.py
+++ tarjeta.py
@@ -171,24 +171,3 @@ class TarjetaMedioBoleto (Tarjeta):
 				else:
 				    print ("Saldo Insuficiente")
 				    return False
-
-
-tarj = TarjetaComun(1)
-tarj.RecargaTarjeta(70)
-tarj.Saldo()
-    
-lak = Colectivo (1,"semtur", "K")
-un122 = Colectivo (2, "semtur", "122v")
-
-tarj.PagarBoleto (un122, "15/10/2014 21:00") #Normal
-tarj.Saldo()
-tarj.PagarBoleto (lak, "15/10/2014 22:00") #Trasbordo
-tarj.Saldo()
-tarj.PagarBoleto (un122, "15/10/2014 22:15") #Normal
-tarj.Saldo()
-tarj.PagarBoleto (lak, "15/10/2014 22:55") #Trasbordo
-tarj.Saldo()
-tarj.PagarBoleto (lak, "15/10/2014 22:56") #Normal
-tarj.Saldo()
-
-tarj.ViajesRealizados()
