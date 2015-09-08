8e2e2408800d1cbb5a0c1349f9e1c15801826f5d
PelukM <marcos.medina.96@gmail.com>
2015-09-08 17:11:16 -0300

    Update test_testtarjeta.py

--- test_testtarjeta.py
+++ test_testtarjeta.py
@@ -181,11 +181,11 @@ def test_Saldo1():
 def test_RecargaTarjeta1():
 	assert tarj.RecargaTarjeta(70) == 70
 	assert tarj.RecargaTarjeta (196) == 230
-	assert tarjM.RecargaTarjeta (368) == 460
+	assert tarj.RecargaTarjeta (368) == 460
 
 K1 = Colectivo (1,"Semtur","K")
 v122= Colectivo(2,"Semtur","122v")
 
 def test_PagarBoleto1():
-	assert tarj.PagarBoleto(v122,"06/07/2008 04:20") == False
+	assert tarjM.PagarBoleto(v122,"06/07/2008 04:20") == False
 	
