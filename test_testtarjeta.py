341a4a8b0ad89bb4a501ee06d89ab8dc474b6d15
PelukM <marcos.medina.96@gmail.com>
2015-09-08 03:44:38 -0300

    Update test_testtarjeta.py

--- test_testtarjeta.py
+++ test_testtarjeta.py
@@ -185,18 +185,4 @@ def test_RecargaTarjeta1():
 	assert RecargaTarjeta (368) == 460
 
 
-lak = Colectivo (1,"semtur", "K")
-un122 = Colectivo (2, "semtur", "122v")
 
-tarj.PagarBoleto (un122, "15/10/2014 21:00") #Normal
-tarj.Saldo()
-tarj.PagarBoleto (lak, "15/10/2014 22:00") #Trasbordo
-tarj.Saldo()
-tarj.PagarBoleto (un122, "15/10/2014 22:15") #Normal
-tarj.Saldo()
-tarj.PagarBoleto (lak, "15/10/2014 22:55") #Trasbordo
-tarj.Saldo()
-tarj.PagarBoleto (lak, "15/10/2014 22:56") #Normal
-tarj.Saldo()
-
-tarj.ViajesRealizados()
