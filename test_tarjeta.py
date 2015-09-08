f0a9322de6ef0f5efd70f27ca07489ab702e537d
PelukM <marcos.medina.96@gmail.com>
2015-09-08 18:29:24 -0300

    Update test_tarjeta.py

--- test_tarjeta.py
+++ test_tarjeta.py
@@ -15,6 +15,10 @@ def test_RecargaTarjeta1():
 	assert tarj.RecargaTarjeta(70) == 70
 	assert tarj.RecargaTarjeta (196) == 230
 	assert tarj.RecargaTarjeta (368) == 460
+	
+def test_RecargaTarjeta2():
+	tarj = TarjetaComun(1)
+	tarjM = TarjetaMedioBoleto(2)
 	tarj.RecargaTarjeta(70)
 	tarjM.RecargaTarjeta(35)
 	assert tarj.saldo == 70
